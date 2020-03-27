18fdee0004c43cddbf6f3e8fcdbfa8039decdb96
Alexina Coullandreau <mozart38@gmail.com>
2020-03-27 10:50:57 +0100

    Refactored adding a new SceneElements class to manage scene elements properties separately. Currently works with the background image only.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 window.onload = function () {
 	// var button = document.getElementsByName('Animate').item(0);
 	// button.addEventListener("click", onButtonClick)
-	scene.background_image = document.getElementById('bg-img');
+	// scene.background_image = document.getElementById('bg-img');
+	// scene.train_image = document.getElementById('bg-train-img');
+	scene.bg_element.image = document.getElementById('bg-img');
+	scene.train_element.image = document.getElementById('bg-train-img');
 	document.addEventListener("keydown", scene.onKeyDown.bind(scene));
 	window.requestAnimationFrame(scene.animate.bind(scene));
 }
@@ -10,36 +13,48 @@ var scene = new Scene()
 
 function Scene() {
 	this.background_image
-	this.animation_duration = 1000
-	this.current_position = 0
+	this.train_image
+	this.train_element = new SceneElements(1000)
+	this.bg_element = new SceneElements(1000)
 	this.last_time = 0
-	this.animation = new Animation(this.current_position, 0, this.animation_duration)
-	this.slideTo = function(position) {
-		this.background_image.style.transform = "translateX(-"+position+"px)";
+	this.slideTo = function(object, position) {
+		object.image.style.transform = "translateX(-"+position+"px)";
 	}
 	this.animate = function(global_time) {
 		window.requestAnimationFrame(this.animate.bind(this));
 		var delta_time = global_time - this.last_time
 		this.last_time = global_time
-		this.animation.animate(delta_time)
+		this.bg_element.animation.animate(this.bg_element, delta_time)
 	}
 	this.onKeyDown = function(event) {
 		var to_pixel = undefined
 		if (event.code === "ArrowDown") {
 			to_pixel = 500;
 		} else if (event.code === "ArrowUp") {
-			to_pixel = 3500;
+			to_pixel = 6000;
 		} else if (event.code === "ArrowLeft") {
 			to_pixel = 1500;
 		} else if (event.code === "ArrowRight") {
 			to_pixel = 0;
 		}
 		if (to_pixel !== undefined) {
-			scene.animation = new Animation(this.current_position, to_pixel, this.animation_duration)
+			scene.bg_element.update_animation(to_pixel)
 		}
 	}
 }
 
+function SceneElements(animation_duration) {
+	this.animation_duration = animation_duration
+	this.image
+	this.current_position = 0
+	this.animation = new Animation(this.current_position, 0, this.animation_duration)
+	this.update_animation = function (to_pixel) {
+		this.animation = new Animation(this.current_position, to_pixel, this.animation_duration)
+	}
+	this.slideTo = function(position) {
+		this.image.style.transform = "translateX(-"+position+"px)";
+	}
+}
 
 function Animation(from_pixel, to_pixel, animation_duration) {
   this.from_pixel = from_pixel;
@@ -51,12 +66,12 @@ function Animation(from_pixel, to_pixel, animation_duration) {
   	var next_position = (dist_to_move / this.animation_duration) * this.time_elapsed
   	return next_position
   };
-  this.animate = function(delta_time) {
+  this.animate = function(object, delta_time) {
   	this.time_elapsed += delta_time
   	if (this.time_elapsed < this.animation_duration) {
   		var offset_pixels = this.offset_pixels()
-  		scene.slideTo(this.from_pixel + offset_pixels);
-  		scene.current_position = this.from_pixel + offset_pixels;
+  		object.slideTo(this.from_pixel + offset_pixels);
+  		object.current_position = this.from_pixel + offset_pixels;
   	}
   }
 }
